32c1d46cd155b6ebd555837654976ae5a1d24891
Roger Davila <rogerddavila@gmail.com>
2022-08-23 16:10:17 -0700

    send pokemon sprite message when proper command is executed

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const pokemonAPI = require('./utils/pokemon-api');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.commands = new Collection();
 
@@ -13,9 +14,9 @@ const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('
 // Set the js files as commands for the client
 // Uses the file path to point to the command
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  client.commands.set(command.data.name, command);
 }
 
 client.once('ready', () => console.log(`Logged in as ${client.user.tag}!`));
@@ -33,6 +34,17 @@ client.on('interactionCreate', async (interaction) => {
     console.error(error);
     await interaction.reply({ content: 'There was an error with executing this command!', ephemeral: true })
   }
+});
+
+client.on('messageCreate', async (message) => {
+  if (!message.content.startsWith('!ps ')) return;
+  let commands = message.content.replace('!ps ', '');
+  commands = commands.split(' ');
+  console.log(commands);
+  // Currently responds with an error if the pokemon might have a different form like Deoxys and Giratina
+  const pokemon = await pokemonAPI.getPokemon(commands[0]);
+  console.log(pokemon);
+  message.channel.send(pokemon.sprites.front_default);
 });
 
 require('./server');
