e69f8f237a51c5c33155db008126536334815f6e
Roger Davila <rogerddavila@gmail.com>
2022-08-23 16:01:48 -0700

    add boilerplate function for GET requests

--- utils/send-request.js
+++ utils/send-request.js
@@ -1,7 +1,11 @@
 // All requests for this API are GET requests
 // No other headers required
-export default async function sendRequest(url) {
+async function sendRequest(url) {
   const res = await fetch(url);
   if (res.ok) return res.json();
   throw new Error('Bad Request');
+}
+
+module.exports = {
+  sendRequest
 }
