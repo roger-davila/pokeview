c8ced296eb3d7c3f07462d9ca16d81db74fa86de
Roger Davila <rogerddavila@gmail.com>
2022-08-23 15:58:39 -0700

    get pokemon data by name

--- utils/pokemon-api.js
+++ utils/pokemon-api.js
@@ -1,9 +1,11 @@
 // Returns general pokemon data
-import { sendRequest } from './send-request';
+const sendRequest = require('./send-request');
 const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
-export function getPokemon(name) {
-  return sendRequest(`${BASE_URL}/${name.toLowerCase()}/`); // Name needs to be spelled out correctly
+function getPokemon(name) {
+  return sendRequest.sendRequest(`${BASE_URL}/${name.toLowerCase()}/`); // Name needs to be spelled out correctly
 }
 
-
+module.exports = {
+  getPokemon
+}
