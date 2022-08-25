c6f615437b349d9045405132a1a423246e7f1437
Roger Davila <rogerddavila@gmail.com>
2022-08-24 23:01:02 -0700

    add data parser for json response

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -1,14 +1,33 @@
+const pokemonAPI = require('../utils/pokemon-api');
 const { SlashCommandBuilder } = require('discord.js');
+const pokemonEmbed = require('../embeds/pokemon');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('pokemon')
     .setDescription('Get Pokemon Sprite')
-    .addStringOption((option) => 
+    .addStringOption((option) =>
       option.setName('pokemon')
-      .setDescription('Pokemon Name')
+        .setDescription('Pokemon Name')
     ),
   async execute(interaction) {
-    console.log('Command was run.');
+    console.log(interaction);
+    console.log(interaction.channelId);
+    const pokemonName = interaction.options.getString('pokemon');
+    console.log(pokemonName);
+    const pokemon = await pokemonAPI.getPokemon(pokemonName);
+    await interaction.reply({ embeds: [pokemonEmbed.embedBuilder(parsePokemonData(pokemon))] });
   },
 };
+
+function parsePokemonData(pokemon) {
+  const newPokemon = {
+    id: pokemon.id,
+    name: pokemon.name,
+    stats: (() => pokemon.stats.reduce((obj, stat) => (obj[stat.stat.name] = stat.base_stat, obj), {}))(),
+    types: (() => pokemon.types.reduce((obj, type) => (obj[type.slot] = type.type.name, obj), {}))(),
+    sprites: {front: pokemon.sprites.front_default, frontShiny: pokemon.sprites.front_shiny}
+  }
+  console.log(newPokemon);
+  return newPokemon;
+}
