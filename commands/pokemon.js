d27f6117b878261f32baae445181f0204f36bee8
Roger Davila <rogerddavila@gmail.com>
2022-08-25 20:41:45 -0700

    change command names and add shiny option

--- commands/pokemon.js
+++ commands/pokemon.js
@@ -4,29 +4,36 @@ const pokemonEmbed = require('../embeds/pokemon');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('pokemon')
-    .setDescription('Get Pokemon Sprite')
+    .setName('search')
+    .setDescription('Get Pokemon information')
     .addStringOption((option) =>
-      option.setName('pokemon')
+      option.setName('name')
         .setDescription('Pokemon Name')
-    ),
+        .setRequired(true))
+    .addBooleanOption((option) =>
+      option.setName('shiny')
+        .setDescription('Show shiny sprite')
+        .setRequired(true)),
   async execute(interaction) {
     console.log(interaction);
     console.log(interaction.channelId);
-    const pokemonName = interaction.options.getString('pokemon');
+    const pokemonName = interaction.options.getString('name');
+    const isShiny = interaction.options.getBoolean('shiny');
     console.log(pokemonName);
+    console.log(isShiny);
     const pokemon = await pokemonAPI.getPokemon(pokemonName);
-    await interaction.reply({ embeds: [pokemonEmbed.embedBuilder(parsePokemonData(pokemon))] });
+    await interaction.reply({ embeds: [pokemonEmbed.embedBuilder(parsePokemonData(pokemon), isShiny)] });
   },
 };
 
 function parsePokemonData(pokemon) {
+  // Instantiate new pokemon object if it does not already exist in the system.
   const newPokemon = {
     id: pokemon.id,
     name: pokemon.name,
     stats: (() => pokemon.stats.reduce((obj, stat) => (obj[stat.stat.name] = stat.base_stat, obj), {}))(),
     types: (() => pokemon.types.reduce((obj, type) => (obj[type.slot] = type.type.name, obj), {}))(),
-    sprites: {front: pokemon.sprites.front_default, frontShiny: pokemon.sprites.front_shiny}
+    sprites: { front: pokemon.sprites.front_default, frontShiny: pokemon.sprites.front_shiny }
   }
   console.log(newPokemon);
   return newPokemon;
