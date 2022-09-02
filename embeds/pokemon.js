d28c2a83b54903425a7db3344947c7a08802a9bf
Roger Davila <rogerddavila@gmail.com>
2022-09-01 22:17:07 -0700

    clean up template formatting

--- embeds/pokemon.js
+++ embeds/pokemon.js
@@ -4,7 +4,8 @@ const { EmbedBuilder } = require('discord.js');
 const BLOCK_UNIT = 32; // Divide stats by this to represent stat value as blocks.
 
 // Stat emoji and format lookup
-const emojiMap = new Map ();
+// ['Display', :emoji:]
+const emojiMap = new Map();
 emojiMap.set('hp', ['HP', ':heart:']);
 emojiMap.set('attack', ['Attack', ':boom:']);
 emojiMap.set('defense', ['Defense', ':shield:']);
@@ -17,15 +18,20 @@ function embedBuilder(pokemon, isShiny) {
     .setColor(0xFF001A)
     .setTitle(stringy.properCase(pokemon.name))
     .setImage(isShiny ? pokemon.sprites.frontShiny : pokemon.sprites.front)
-    .setFields({ 
-      name: 'Base Stats :books:',
-      value: Object.entries(pokemon.stats).reduce((template, stat) => template += `${emojiMap.get(stat[0])[1]} **${emojiMap.get(stat[0])[0]}:** ${stat[1]}\n ${':green_square: '.repeat(Math.ceil(stat[1]/BLOCK_UNIT)) + ':black_large_square: '.repeat(8 - Math.ceil(stat[1]/BLOCK_UNIT))}\n\n`, ``), 
-      inline: true 
-    })
-    .setFooter({text: `Base stat values range from 1 - 255`});
+    .setDescription('**Base Stats** :books:')
+    .setFields(mutateStatsToFieldObject(pokemon))
+    .setFooter({ text: `Base stat values range from 1 - 255` });
   return pokemonEmbed;
 }
 
 module.exports = {
   embedBuilder
+}
+
+function mutateStatsToFieldObject(pokemon) {
+  return Object.entries(pokemon.stats).map((stat) => {
+    return { 
+      name: `${emojiMap.get(stat[0])[1]} **${emojiMap.get(stat[0])[0]}:** \`${stat[1]}\`​`, 
+      value: `${':green_square: '.repeat(Math.ceil(stat[1] / BLOCK_UNIT))} ${':black_large_square: '.repeat(8 - Math.ceil(stat[1] / BLOCK_UNIT))}`
+    }});
 }
